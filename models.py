fd09ebbce0d9ecfc6ef92959b8254a1d408f8b72
vivianrodrigues-dev <vivianrodriguessszz@gmail.com>
2026-03-30 10:29:41 -0300

    Toques finais pt2

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 import services
-#import pandas as pd
-#import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def validar_escolha(escolha):
     if escolha in [0,1,2,3,4,5,6,7,8,9]:
